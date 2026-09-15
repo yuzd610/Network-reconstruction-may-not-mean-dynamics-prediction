3c14eb25d00e66df5ad72f4ed8f572dd208c3fc9
Z.D.Yu <yuzd610@163.com>
2026-09-15 18:46:31 +0800

    Update 4DMFT map in machine learning.py

--- Network reconstruction may not mean dynamics prediction/The effect of DMFT in machine learning/g=0.8/4DMFT map in machine learning.py
+++ Network reconstruction may not mean dynamics prediction/The effect of DMFT in machine learning/g=0.8/4DMFT map in machine learning.py
@@ -112,8 +112,8 @@ plt.rcParams['ytick.labelsize'] = 23
 
 
 
-plt.plot(time, d, label=r'DMFT', linestyle='solid', color='red',linewidth=3)  # 将虚线改为实线并设置为红色
-plt.plot(time, mean_d_n, label=r'simulation', linestyle='dashed', color='blue',linewidth=3)  # 设置为蓝色虚线
+plt.plot(time, d, label=r'DMFT', linestyle='solid', color='red',linewidth=3)  
+plt.plot(time, mean_d_n, label=r'simulation', linestyle='dashed', color='blue',linewidth=3)  
 plt.fill_between(time, mean_d_n - std_d_n, mean_d_n + std_d_n, color='blue', alpha=0.2)
 # Set axis labels
 plt.xlabel('Time ', fontsize=23)
